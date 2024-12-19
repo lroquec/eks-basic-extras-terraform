20e8092af1a409a55efcec4014d9c7566a82e18c
Laura Roque <lroquecu@gmail.com>
2024-12-19 10:50:27 +0100

    add helm output

--- outpusts.tf
+++ outpusts.tf
@@ -2,3 +2,9 @@ output "lbc_iam_policy" {
   #value = data.http.lbc_iam_policy.body
   value = data.http.lbc_iam_policy.response_body
 }
+
+# Helm Release Outputs
+output "lbc_helm_metadata" {
+  description = "Metadata Block outlining status of the deployed release."
+  value = helm_release.loadbalancer_controller.metadata
+}
