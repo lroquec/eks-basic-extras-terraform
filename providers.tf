88018a3121bbc80fbc95873d9433bbef9dd2b4a6
Laura Roque <lroquecu@gmail.com>
2024-12-19 10:48:55 +0100

    add helm

--- providers.tf
+++ providers.tf
@@ -49,3 +49,12 @@ provider "aws" {
 provider "http" {
   # Configuration options
 }
+
+# HELM Provider
+provider "helm" {
+  kubernetes {
+    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
