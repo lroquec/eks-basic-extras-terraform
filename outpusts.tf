b9c223d3ac84effff0eeb331deda42eefa47a29c
Laura Roque <lroquecu@gmail.com>
2024-12-19 14:39:50 +0100

    add cluster autoscaler

--- outpusts.tf
+++ outpusts.tf
@@ -14,3 +14,9 @@ output "externaldns_helm_metadata" {
   description = "Metadata Block outlining status of the deployed release."
   value       = helm_release.external_dns.metadata
 }
+
+# Helm Release Outputs
+output "cluster_autoscaler_helm_metadata" {
+  description = "Metadata Block outlining status of the deployed release."
+  value       = helm_release.cluster_autoscaler_release.metadata
+}
--- variables.tf
+++ variables.tf
@@ -15,5 +15,5 @@ variable "environment" {
 variable "business_division" {
   description = "Business Division in the large organization this Infrastructure belongs"
   type        = string
-  default     = "Billing"
+  default     = "billing"
 }
