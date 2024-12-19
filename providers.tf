045b4666a9ab3fb2d45cbcda78a08d867623aa97
Laura Roque <lroquecu@gmail.com>
2024-12-19 10:50:59 +0100

    add kubernetes provider

--- providers.tf
+++ providers.tf
@@ -58,3 +58,10 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
+
+# Terraform Kubernetes Provider
+provider "kubernetes" {
+  host = data.terraform_remote_state.eks.outputs.cluster_endpoint 
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  token = data.aws_eks_cluster_auth.cluster.token
+}
