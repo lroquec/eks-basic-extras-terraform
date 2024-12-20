5dcb169a69971059589c57c5181f1033760ffc64
Laura Roque <lroquecu@gmail.com>
2024-12-20 11:07:50 +0100

    add metrics server

--- outpusts.tf
+++ outpusts.tf
@@ -20,3 +20,9 @@ output "cluster_autoscaler_helm_metadata" {
   description = "Metadata Block outlining status of the deployed release."
   value       = helm_release.cluster_autoscaler_release.metadata
 }
+
+# Helm Release Outputs
+output "metrics_server_helm_metadata" {
+  description = "Metadata Block outlining status of the deployed release."
+  value = helm_release.metrics_server_release.metadata
+}
